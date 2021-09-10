863ebbab50d95441e13fdf29285082cd1d8c168d
Martynas Balaišis <36953505+Marchyus@users.noreply.github.com>
2021-09-10 09:37:49 +0300

    Update Clean-audit-log.sql
    Added description

--- Clean-audit-log.sql
+++ Clean-audit-log.sql
@@ -1,5 +1,20 @@
 --
--- Cleanup audit log BPAAuditEvents, by writing NULLs into oldXML and newXML columns. 
+-- Cleanups audit log BPAAuditEvents, by writing NULLs into oldXML and newXML columns. 
+-- Tested with BluePrism 6.*
+-- 
+-- Main difference from cleanup script, provided by BluePrism, is this:
+-- - Does not lock out table (if updating >5000 rows)
+-- - Can be executed on active database
+-- - Can be executed in small batches
+-- - Can be executed on huge tables (tested with >250GB)
+--
+-- How to/before executing. Set values to:
+-- @daystokeep - days to keep. Default value - 365 days.
+-- @maxloops - how many update loops to perform
+-- @rowcount - how many recors to update in one loop
+--
+-- To calculate ammount of rows will be updated, multiplicate maxloops with row count (e.g. 100 * 100 = 10K rows will be updated)
+-- 
 --
 -- IMPORTANT! Set how many days of process edit history to keep.
 DECLARE @daystokeep int;
